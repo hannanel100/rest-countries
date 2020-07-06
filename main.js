8d6ee13f2eed7948ad5b4fcda2d228ccdf0db571
hannanel gershinsky <hannanel100@gmail.com>
2020-07-06 20:25:54 +0300

    done

--- main.js
+++ main.js
@@ -3,64 +3,60 @@ const countryName = document.getElementById("country-name");
 const content = document.getElementById("content");
 let countryDiv = "";
 
-all.addEventListener("click", function (e) {
-    e.preventDefault();
-    const url = 'https://restcountries.eu/rest/v2/all';
-    sendToCountries(url);
+all.addEventListener("click", e => {
+  e.preventDefault();
+  const url = "https://restcountries.eu/rest/v2/all/?fields=name;capital;currencies;topLevelDomain;flag";
+  fetchCountries(url);
+});
 
-})
-countryName.addEventListener("click", function (e) {
-    e.preventDefault();
-    const countryInput = document.getElementById("country-input");
-    const country = countryInput.value;
-    const url = 'https://restcountries.eu/rest/v2/name/' + country;
-    sendToCountries(url);
+countryName.addEventListener("click", e => {
+  e.preventDefault();
+  const countryInput = document.getElementById("country-input");
+  const country = countryInput.value;
+  const url = `https://restcountries.eu/rest/v2/name/${country}/?fields=name;capital;currencies;topLevelDomain;flag`;
+  fetchCountries(url);
+});
 
-})
-function reqListener() {
-    var objCountries = JSON.parse(this.responseText);
-    buildPage(objCountries);
-}
+const fetchCountries = async url => {
+  const response = await fetch(url);
+  const data = await response.json();
+  if (data.status === 404) {
+    content.innerHTML = "<h2 class='error'>Country Not Found</h2>";
+  } else {
+    buildPage(data);
+  }
+};
 
-function sendToCountries(url) {
-    var oReq = new XMLHttpRequest();
-    oReq.addEventListener("load", reqListener);
-    oReq.open("GET", url);
-    oReq.onloadend = function () {
-        if (oReq.status == 404) {
-            let textDiv = buildDiv("error");
-            textDiv.setAttribute("class", "error");
-            textDiv.innerHTML = "Country Not found!";
-            content.appendChild(textDiv);
-        }
-    }
-    oReq.send();
-}
-function buildPage(countries) {
-    content.innerHTML = '';
-    const inputContent = document.getElementById("country-input");
-    inputContent.value = "";
-    document.getElementById("country-input").value.innertext = "";
-    for (let i = 0; i < countries.length; i++) {
-        countryDiv = buildDiv("country");
-        countryDiv.setAttribute("class", "row country");
-        const textDiv = buildDiv("text-cl");
-        textDiv.setAttribute("class", "col-sm text-cl");
-        const imgDiv = buildDiv("img-cl");
-        imgDiv.setAttribute("class", "col-sm img-cl");
-        textDiv.innerHTML += "<div>Name: " + countries[i].name + "</div>";
-        textDiv.innerHTML += "<div>Top Leve Domain: " + countries[i].topLevelDomain + "</div>";
-        textDiv.innerHTML += "<div>Capital: " + countries[i].capital + "</div>";
-        textDiv.innerHTML += "<div>Currencies: " + JSON.stringify(countries[i].currencies) + "</div>";
-        countryDiv.appendChild(textDiv);
-        imgDiv.innerHTML += "<img src=" + countries[i].flag + " class='rounded img-fluid'>";
-        countryDiv.appendChild(imgDiv);
-        content.appendChild(countryDiv);
-    }
-}
+const buildPage = countries => {
+  content.innerHTML = "";
+  const inputContent = document.getElementById("country-input");
+  inputContent.value = "";
+  document.getElementById("country-input").value.innertext = "";
+  const countriesCards = countries.map(country => {
+    return buildCard(country);
+  });
+  content.innerHTML = countriesCards;
+};
 
-function buildDiv(cl) {
-    const divElement = document.createElement('div');
-    divElement.setAttribute('class', cl);
-    return divElement;
-}
+const buildCard = country => {
+  const currency = country.currencies;
+  const currencyCode = currency.map(curr => curr.code);
+  const currencyName = currency.map(curr => curr.name);
+  const currencySymbol = currency.map(curr => curr.symbol);
+  const currencyDiv = `<div class="currency"><p>Code: ${currencyCode.map(
+    code => code,
+  )}</p> <p>Name: ${currencyName.map(
+    name => name,
+  )} </p> <p>Symbol: ${currencySymbol.map(symbol => symbol)}</p></div>`;
+  const countryCard = `<div class="card" style="width: 18rem;">
+        <img src=${country.flag} class="card-img-top" alt="country flag...">
+        <div class="card-body">
+          <h4 class="card-title">Name: ${country.name}</h4>
+          <h5 class="card-text">Top Level Domain: ${country.topLevelDomain}</h5>
+          <h5 class="card-text">Capital: ${country.capital}</h5>
+          <h5 class="card-text">Currencies:  </h5>
+          ${currencyDiv}
+        </div>
+      </div>`;
+  return countryCard;
+};
